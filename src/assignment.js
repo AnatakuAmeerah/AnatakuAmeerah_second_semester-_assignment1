a9a8ed110b9d356510b653ebd2b5c97f9a1f3ba2
Anataku Ameerah <anatakuameerah@gmail.com>
2023-08-30 18:49:30 +0000

    fourth-trial

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i = 0; i <= destination; i++){
+        sum += i;
+        }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+ assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -68,11 +71,16 @@ function countEvenNumbersWithin(destination) {
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
     let result = [];
+    for (let i = 0; i < arrayOfNumbers.length; i++){
+        let fahrenheitTemp= Math.trunc((arrayOfNumbers[i] * 9/5) + 32);
+        result.push(fahrenheitTemp);
+    
+    }
 
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
